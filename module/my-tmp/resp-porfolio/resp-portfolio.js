10e60ee8a6323bf845f469f14c9aa3e912f773db
wgsam <samwong@smartblock.pro>
2019-03-28 14:51:36 +0800

    add responsive portfolio tmp

---
+++ module/my-tmp/resp-porfolio/resp-portfolio.js
@@ -0,0 +1,12 @@
+ScrollReveal().reveal('.anim', {
+    // delay: 375,
+    // distance: '10%',
+    // easing: 'ease-in',
+    // reset: true
+
+    origin: 'bottom',
+    delay: 100,
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true
+});
