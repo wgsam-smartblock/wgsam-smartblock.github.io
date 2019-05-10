bac816ac6a684fdc28ca93530a6ba2eba6399aa6
wgsam <samwong@smartblock.pro>
2019-05-10 17:49:28 +0800

    add iSMART project

---
+++ module/project/iSMART-mobile-style/iSMART.js
@@ -0,0 +1,42 @@
+// Read ME
+
+// ----- Company Page -----
+
+// header
+// .screen --> for full background color
+// .navbar-header --> header color
+// .title --> header title font color
+
+// header wallpaper
+// .header-wallpaper .bck-img --> chg background image in this format -> background: url("img/lanscape.jpg") center/cover no-repeat;
+// .header-wallpaper .info .title --> chg font color
+// .liveChat --> chage tab background color and font color
+// .contactUs --> chage tab background color and font color
+
+// data list
+// data in block format --> use this css (.data-list.block .item)
+// data in list format --> use this css (.data-list.list .item)
+
+
+// ----- Member Page -----
+
+// .screen --> for full background color
+// .navbar-header --> header color
+
+// header wallpaper
+// .header-wallpaper .bck-img --> chg background image in this format -> background: url("img/lanscape.jpg") center/cover no-repeat;
+// .header-wallpaper .info .title --> chg font color
+
+
+// ----- Landing Page -----
+// .screen --> for full background color
+// .navbar-header --> header color
+// .left-icon --> chg icon 
+// .right-icon --> chg icon 
+// .header-img .middle-img --> chg header image
+
+//.info-content-top , .footer-wraper 
+//.top-wrapper, .info-content-bottom
+
+// .tab --> change background-color
+// .tab:hover --> change hover background-color
