ac66f2afb6953bb7a084067f4a68a3eb67d5e60f
wgsam <samwong@smartblock.pro>
2019-07-29 11:16:21 +0800

    update flag svg animation

--- module/svg/flag-svg/flag-svg.js
+++ module/svg/flag-svg/flag-svg.js
@@ -10,4 +10,14 @@ window.onload = function () {
         $('.flag').append(flagElement);
     }
 
+    var h = $('.flag2').width();
+    for (var i = 0; i < h; i++) {
+        var flagElement = $("<div class='flag-element2'>");
+        flagElement.css('background-position', -i + "px 0");
+        flagElement.css('-webkit-animation-delay', i * 30 + 'ms');
+        flagElement.css('-moz-animation-delay', i * 30 + 'ms');
+        flagElement.css('-ms-animation-delay', i * 30 + 'ms');
+        flagElement.css('animation-delay', i * 30 + 'ms');
+        $('.flag2').append(flagElement);
+    }
 }
