686e673f3a7758fbd3e141081f3b580a59d04935
wgsam <samwong@smartblock.pro>
2019-10-17 15:30:56 +0800

    upload checkAnimaiton - Lottie (Bodymovin AE)

---
+++ module/alert/testing-check/checkAnimaiton.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('checkAnimaiton'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'checkAnimation.json'
+  })
