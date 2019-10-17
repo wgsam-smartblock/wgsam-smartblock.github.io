ea8f71383e0f4d75a416c333c443c42096e2d20a
wgsam <samwong@smartblock.pro>
2019-10-17 17:33:19 +0800

    update lottie bodymovin animation alert

--- module/alert/testing-check/checkAnimaiton.js
+++
@@ -1,7 +0,0 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('checkAnimaiton'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'checkAnimation.json'
-  })
